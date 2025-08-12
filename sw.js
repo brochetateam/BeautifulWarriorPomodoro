d06701f006ba4f2530ea193b6094318bc070eb8b
Andres Cervantes Lopez <brochetateam@gmail.com>
2025-08-12 19:01:31 +0200

    PWA - SVG - Movil

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'bw-pomodoro-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/cat-192.png',
+  './icons/cat-512.png',
+  './icons/cat-maskable-192.png',
+  './icons/cat-maskable-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((cache) => cache.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((r) => r || caches.match('./')))
+  );
+});
